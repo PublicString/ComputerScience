ac4d9f63df3e50642bde99f44aac5e7d626c40bd
PublicString <BG357@student.anglia.ac.uk>
2015-10-16 02:16:38 +0100

    Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 7, 4, 6 };
+
+            for (int i = 0; i <= (array.Length) - 1; i++)
+            {
+                Thread.Sleep(200);
+                Console.WriteLine("Array: {0}", array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
