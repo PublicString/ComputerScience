004bad650a3f5614bece8ccf81c24c4d833a5626
PublicString <BG357@student.anglia.ac.uk>
2015-10-16 02:25:34 +0100

    Arrays

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -7,12 +7,14 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 7, 4, 6 };
+            int[] array = { 1, 7, 4, 6 } ;
+            int[] array2 = { 18, 4, 74, 8 };
 
             for (int i = 0; i <= (array.Length) - 1; i++)
             {
                 Thread.Sleep(200);
-                Console.WriteLine("Array: {0}", array[i]);
+                //Console.WriteLine("Array: " + array[i] + "x" + array2[i] + " = " + (array[i] * array2[i]));
+                Console.WriteLine("Array: {0}x{1} = {2}", array[i], array2[i], array[i] * array2[i]);
             }
             Console.ReadLine();
         }
